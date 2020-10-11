028eb80fce763c5a51d8f879cbc7cf070c4a0fd0
Ivan Godenov <gizomo@ya.ru>
2020-10-11 17:34:14 +0700

    master

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CardComponent } from './catalog/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
